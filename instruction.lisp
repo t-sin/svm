8f51c299f57f11d0fd582f86c695027de49deeb5
t-sin <shinichi.tanaka45@gmail.com>
2018-06-03 00:45:51 +0900

    WIP: instraction specs

---
+++ instruction.lisp
@@ -0,0 +1,11 @@
+(in-package #:cl-user)
+(defpackage #:svm-ins
+  (:use #:cl)
+  (:export #:+opcode-specs+))
+(in-package #:svm-ins)
+
+(defmacro defop (name) () &body)))
+
+(defop :add ()
+  ((opr1 :type :int)
+   (opr2 :type :int)))
---
+++ svm-ins.asd
@@ -0,0 +1,7 @@
+(defsystem #:svm-ins
+  :class :package-inferred-system
+  :description "String VM instructions"
+  :version "0.1"
+  :author "TANAKA Shinichi"
+  :license "LLGPL"
+  :depends-on ("svm-ins/instruction"))
