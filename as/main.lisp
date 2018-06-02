1147e6e4092717c061e2053e746f8f5c30912715
t-sin <shinichi.tanaka45@gmail.com>
2018-06-02 14:18:12 +0900

    Adds assembler

---
+++ as/main.lisp
@@ -0,0 +1,28 @@
+(in-package #:cl-user)
+(defpackage #:svm-as/as/main
+  (:use #:cl)
+  (:export #:read-asm))
+(in-package #:svm-as/as/main)
+
+(defun read-data (stream)
+  :data)
+
+(defun read-code (stream)
+  :code)
+
+(defun read-asm (stream)
+  (loop
+    :for line := (read-line stream nil :eof)
+    :with program := nil
+    :until (eq line :eof)
+    :do (let ((trimmed (string-trim '(#\spece #\tab) line)))
+          (cond ((zerop (length trimmed)) nil)
+                ((string= trimmed ".data")
+                 (if (getf program :data)
+                     (error ".data section appears twice!")
+                     (setf (getf program :data) (read-data stream))))
+                ((string= trimmed ".code")
+                 (if (getf program :code)
+                     (error ".code section appears twice!")
+                     (setf (getf program :code) (read-code stream))))
+                (t (error (format nil "this ~s is not section name")))))))
---
+++ svm-as.asd
@@ -0,0 +1,7 @@
+(defsystem #:svm-as
+  :class :package-inferred-system
+  :description "String VM assembler"
+  :version "0.1"
+  :author "TANAKA Shinichi"
+  :license "LLGPL"
+  :depends-on ("svm-as/as/main"))
