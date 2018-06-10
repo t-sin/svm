3e16f113f4a70e67dce79a8e22696c937b570594
t-sin <shinichi.tanaka45@gmail.com>
2018-06-10 15:02:51 +0900

    Fixes package name for exporting

--- as/package.lisp
+++ as/package.lisp
@@ -1,7 +1,7 @@
 (in-package #:cl-user)
-(defpackage #:svm-as/as
+(defpackage #:svm-as
   (:use #:cl)
   (:import-from #:svm-as/as/read
                 #:read-asm)
   (:export #:read-asm))
-(in-package #:svm-as/as)
+(in-package #:svm-as)
