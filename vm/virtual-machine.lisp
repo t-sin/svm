cc6ec84618d84ca5fe480b0e9eb6a2050f17265e
t-sin <shinichi.tanaka45@gmail.com>
2018-06-10 18:40:23 +0900

    Adds convinient function

--- vm/virtual-machine.lisp
+++ vm/virtual-machine.lisp
@@ -1,7 +1,7 @@
 (in-package #:cl-user)
 (defpackage #:svm-vm/vm/virtual-machine
   (:use #:cl)
-  (:export #:make-vm
+  (:export #:make-vm*
            #:dump-vm
            #:step-program
            #:run-program))
@@ -11,6 +11,22 @@
 (defstruct vm
   memory access-mem dump-mem
   pc r0 r1 r2 r3 r4 r5 r6 r7)
+
+(defun make-vm* (memory mem-accessor mem-dumper
+                &optional (pc 0) (r0 0) (r1 0) (r2 0) (r3 0)
+                          (r4 0) (r5 0) (r6 0) (r7 0))
+  (make-vm :memory memory
+           :access-mem mem-accessor
+           :dump-mem mem-dumper
+           :pc pc
+           :r0 r0
+           :r1 r1
+           :r2 r2
+           :r3 r3
+           :r4 r4
+           :r5 r5
+           :r6 r6
+           :r7 r7))
 
 (defun dump-vm (vm))
 
