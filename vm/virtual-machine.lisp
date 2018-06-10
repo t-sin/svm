088882132fcb09860fe6ff17fa5a61d6ac16d2e3
t-sin <shinichi.tanaka45@gmail.com>
2018-06-10 23:27:46 +0900

    WIP: specifies binary data format

--- vm/virtual-machine.lisp
+++ vm/virtual-machine.lisp
@@ -9,7 +9,37 @@
 
 (defstruct vm
   memory access-mem dump-mem
-  pc r0 r1 r2 r3 r4 r5 r6 r7)
+  pc r0 r1 r2 r3 r4 r5 r6 r7
+  "String Virtual Machine.
+
+# Memory
+
+SVM has a memory segmented as bytes. The memory holds data as a single or multiple bytes.
+
+Any things can be a memory satisfies some requirements. These requriments are belows:
+
+- can be created as single data structure
+- can read/write a byte through accessor function
+- can dump through stringify the memory (to debugging or to seriarizing)
+
+# Data representation
+
+All data in SVM have each representation, *based on bytes*.
+
+The smallest data is a byte, but some data types are needed multiple bytes because of single
+byte is too small to express. Multiple-bytes data are coded with ordering **little endian**
+-- least significant byte are placed as left, and most significant byte are placed at left.
+
+## General format
+
+- data table
+- code area
+
+## Binary instruction format
+
+- word length
+
+")
 
 (defun make-vm* (memory mem-accesser mem-dumper
                 &optional (pc 0) (r0 0) (r1 0) (r2 0) (r3 0)
