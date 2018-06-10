b0a0142e7831644bd1f373ee3b11a803e84323d2
t-sin <shinichi.tanaka45@gmail.com>
2018-06-10 22:35:37 +0900

    fixup

--- vm/virtual-machine.lisp
+++ vm/virtual-machine.lisp
@@ -7,12 +7,11 @@
            #:run-program))
 (in-package #:svm-vm/vm/virtual-machine)
 
-
 (defstruct vm
   memory access-mem dump-mem
   pc r0 r1 r2 r3 r4 r5 r6 r7)
 
-(defun make-vm* (memory mem-accessor mem-dumper
+(defun make-vm* (memory mem-accesser mem-dumper
                 &optional (pc 0) (r0 0) (r1 0) (r2 0) (r3 0)
                           (r4 0) (r5 0) (r6 0) (r7 0))
   (make-vm :memory memory
