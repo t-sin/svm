a1d4c31e5715cd53e76325a9eb54357e5e8ba46e
t-sin <shinichi.tanaka45@gmail.com>
2018-06-30 17:39:52 +0900

    Construct internal representation of program, from AST

--- as/program.lisp
+++ as/program.lisp
@@ -42,25 +42,29 @@
 (defun get-type (str)
   (flet ((first-char= (ch) (char= (char str 0) ch))
          (last-char= (ch) (char= (char str (1- (length str))) ch)))
-    (cond ((first-char= #\$) :reg)
-          ((first-char= #\%) :addr)
-          ((first-char= #\&) :const)
+    (cond ((null str) :null)
+          ((first-char= #\$) :reg)
+          ((first-char= #\&) :addr)
+          ((first-char= #\%) :const)
           ((and (first-char= #\") (last-char= #\") :str))
           (t :int))))
 
 (defun internal-repr (str type)
   (ecase type
-    (:reg (intern (subseq str 1) :keyword))
+    (:null nil)
+    (:reg (intern (string-upcase (subseq str 1)) :keyword))
     (:addr (parse-integer (subseq str 1)))
     (:const (intern (string-upcase (subseq str 1)) :keyword))
     (:str (subseq str 1 (1- (length str))))
     (:byte (parse-integer str))
     (:int (parse-integer str))))
 
+(defparameter *name-count* 0)
+
 (defun construct-program (ast)
   (let ((data (make-array 0 :element-type '<data>
                           :adjustable t :fill-pointer 0))
-        (name-pos-table (make-hash-table :test 'eq))
+        (datamap (make-hash-table :test 'eq))
         (code (make-array 0 :element-type '<operation>
                           :adjustable t :fill-pointer 0)))
     (flet ((push-data (name value)
@@ -68,8 +72,30 @@
                     (dat (make-<data> :type type
                                       :value (internal-repr value type)))
                     (pos (vector-push-extend dat data)))
-               (setf (gethash name name-pos-table) pos))))
+               (setf (gethash name datamap) pos))))
       (loop
         :for (name value) :in (getf ast :data)
-        :do (push-data name value))
-      (values ast data name-pos-table code))))
+        :do (push-data name value)))
+    (flet ((parse-and-push-operand (str)
+             (let* ((type (get-type str))
+                    (value (internal-repr str type))
+                    (dat (make-<data> :type type :value value)))
+               (when (member type '(:const :byte :int :str))
+                 (let ((pos (vector-push-extend (internal-repr str type) data))
+                       (name (intern (format nil "$name~a$" *name-count*) :keyword)))
+                   (setf (gethash name datamap) pos)))
+               dat)))
+      (loop
+        :for (opc opr1 opr2 opr3) :in (getf ast :code)
+        :do (format t "~s: ~s ~s ~s~%" opc opr1 opr2 opr3)
+        :do (format t "  ~a ~a ~a~%" (get-type opr1) (get-type opr2) (get-type opr3))
+        :do (let* ((i (find opc +opcode-specs+ :key #'<instruction>-name))
+                   (operand1 (parse-and-push-operand opr1))
+                   (operand2 (parse-and-push-operand opr2))
+                   (operand3 (parse-and-push-operand opr3))
+                   (o (make-<operation> :op i
+                                        :opr1 operand1
+                                        :opr2 operand2
+                                        :opr3 operand3)))
+              (vector-push-extend o code))))
+    (make-<program> :data data :datamap datamap :code code)))
