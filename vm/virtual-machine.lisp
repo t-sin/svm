13d7189d87bb7e08387f8807ed1b25fdffb8158e
t-sin <shinichi.tanaka45@gmail.com>
2018-07-01 22:43:21 +0900

    WIP: program loader

--- vm/virtual-machine.lisp
+++ vm/virtual-machine.lisp
@@ -76,7 +76,7 @@ Operand of each operation consists of *intermediate flag* (first bit) and resist
   memory access-mem dump-mem
   pc r0 r1 r2 r3 r4 r5 r6 r7)
 
-(defun make-vm* (memory mem-accesser mem-dumper
+(defun make-vm* (memory mem-accessor mem-dumper
                 &optional (pc 0) (r0 0) (r1 0) (r2 0) (r3 0)
                           (r4 0) (r5 0) (r6 0) (r7 0))
   (make-vm :memory memory
@@ -92,8 +92,14 @@ Operand of each operation consists of *intermediate flag* (first bit) and resist
            :r6 r6
            :r7 r7))
 
-(defun load (program vm)
-  )
+(defun load-program (program vm)
+  (flet ((vm-read (addr)
+           (funcall (fdefinition (vm-access-mem vm))
+                    (vm-memory vm) addr))
+         (vm-write (addr byte)
+           (funcall (fdefinition `(setf ,(vm-access-mem vm)))
+                    byte (vm-memory vm) addr)))
+    vm))
 
 (defun dump-vm (vm))
 
