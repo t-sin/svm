0f02bd484c3f490b7e6bb5a167cb2821f6e7c4ba
t-sin <shinichi.tanaka45@gmail.com>
2018-07-03 22:10:51 +0900

    Remove :r7 register because I want 8 registers including :pc

--- vm/virtual-machine.lisp
+++ vm/virtual-machine.lisp
@@ -14,11 +14,11 @@
 
 (defstruct <vm>
   memory access-mem dump-mem
-  pc r0 r1 r2 r3 r4 r5 r6 r7)
+  pc r0 r1 r2 r3 r4 r5 r6)
 
 (defun make-vm (memory mem-accessor mem-dumper
                 &optional (pc 0) (r0 0) (r1 0) (r2 0) (r3 0)
-                  (r4 0) (r5 0) (r6 0) (r7 0))
+                  (r4 0) (r5 0) (r6 0))
   (make-<vm> :memory memory
              :access-mem mem-accessor
              :dump-mem mem-dumper
@@ -29,8 +29,7 @@
              :r3 r3
              :r4 r4
              :r5 r5
-             :r6 r6
-             :r7 r7))
+             :r6 r6))
 
 (defun encode-data (data)
   (let ((val (<data>-value data)))
