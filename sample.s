ac31b2bc0280a425e02f00a20a4e8918c16cab82
t-sin <shinichi.tanaka45@gmail.com>
2018-07-06 08:09:51 +0900

    Removes ros script: GitHub watcher has gone

--- sample.s
+++ sample.s
@@ -9,5 +9,10 @@
     load 43 $r1        ; load from immediate
     add $r1 $r2 $r3    ; add 42 + 43
     store $r3 &400     ; store into addr (400)
+
+    load 20 $4
+    load 21 $5
+    ifeq $r4 $5 :END   ; junp to :END if 20 ($4) is equal to 21 ($r5)
     hw                 ; hello assembly world!
+:END
     exit               ; exit program
