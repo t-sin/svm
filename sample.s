fbd2f502a967439baffcd7ed8190fadbfb52aa5c
t-sin <shinichi.tanaka45@gmail.com>
2018-07-07 00:51:51 +0900

    Resister names are like $r1, $3 or $5!!!

--- sample.s
+++ sample.s
@@ -10,9 +10,9 @@
     add $r1 $r2 $r3    ; add 42 + 43
     store $r3 &400     ; store into addr (400)
 
-    load 20 $4
-    load 21 $5
-    ifeq $r4 $5 :END   ; junp to :END if 20 ($4) is equal to 21 ($r5)
+    load 20 $r4
+    load 21 $r5
+    ifeq $r4 $r5 :END   ; junp to :END if 20 ($4) is equal to 21 ($r5)
     hw                 ; hello assembly world!
 :END
     exit               ; exit program
