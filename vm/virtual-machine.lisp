bc0b84aa8c0f44ae8e4915512eb9084d1f5e6fc3
t-sin <shinichi.tanaka45@gmail.com>
2018-07-07 22:32:12 +0900

    Cosmetic change

--- vm/virtual-machine.lisp
+++ vm/virtual-machine.lisp
@@ -28,19 +28,9 @@
   pc r0 r1 r2 r3 r4 r5 r6)
 
 (defun make-vm (memory mem-accessor mem-dumper
-                &optional (pc 0) (r0 0) (r1 0) (r2 0) (r3 0)
-                  (r4 0) (r5 0) (r6 0))
-  (make-<vm> :memory memory
-             :access-mem mem-accessor
-             :dump-mem mem-dumper
-             :pc pc
-             :r0 r0
-             :r1 r1
-             :r2 r2
-             :r3 r3
-             :r4 r4
-             :r5 r5
-             :r6 r6))
+                &optional (pc 0) (r0 0) (r1 0) (r2 0) (r3 0) (r4 0) (r5 0) (r6 0))
+  (make-<vm> :memory memory :access-mem mem-accessor :dump-mem mem-dumper
+             :pc pc :r0 r0 :r1 r1 :r2 r2 :r3 r3 :r4 r4 :r5 r5 :r6 r6))
 
 (defun encode-data (data)
   (let ((val (<data>-value data)))
