0e5d0df2074ac38bef09f542f6abff479f38e54b
t-sin <shinichi.tanaka45@gmail.com>
2018-07-07 22:43:07 +0900

    Functinons for easy testing

--- package.lisp
+++ package.lisp
@@ -11,11 +11,30 @@
                 #:load-program
                 #:step-program
                 #:run-program)
+  (:import-from #:svm-vm/vm/simple-memory
+                #:make-simple-memory
+                #:segment
+                #:dump-simple-memory)
   (:export #:read-asm
            #:make-program
            #:make-vm
            #:dump-vm
            #:load-program
            #:step-program
-           #:run-program))
+           #:run-program
+
+           #:make-memory*
+           #:init-vm))
 (in-package #:svm)
+
+(defun make-memory* ()
+  (list (make-simple-memory 1000)
+        'segment
+        #'dump-simple-memory))
+
+(defun init-vm (pathname)
+  (let* ((program (with-open-file (in pathname)
+                    (make-program (read-asm in))))
+         (vm (apply #'make-vm (make-memory*))))
+    (load-program program vm)
+    (values program vm)))
