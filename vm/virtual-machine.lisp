d03daf2d2498e6deda3832edeb705a1ee73c514f
t-sin <shinichi.tanaka45@gmail.com>
2018-07-14 19:19:49 +0900

    LOAD does not read value from memory

--- vm/virtual-machine.lisp
+++ vm/virtual-machine.lisp
@@ -170,8 +170,8 @@
       (:shr (format t "(ash ~s -~s) and store into ~s~%" operand1 operand2 operand3))
       (:add (format t "add ~s to ~s and store into ~s~%" operand1 operand2 operand3)
             (setf (slot-value vm (decode-register operand3))
-                  (+ (vm-read vm (slot-value vm (decode-register operand1)))
-                     (vm-read vm (slot-value vm (decode-register operand2))))))
+                  (+ (slot-value vm (decode-register operand1))
+                     (slot-value vm (decode-register operand2)))))
       (:mul (format t "multiply ~s with ~s and store into ~s~%" operand1 operand2 operand3))
       (:div (format t "divide ~s by ~s and store into ~s~%" operand1 operand2 operand3)))))
 
